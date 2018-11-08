bc2779b5ada6a4c8c462df9490f54bf7cad263f7
nieshankun <38554448+nieshankun@users.noreply.github.com>
2018-11-08 23:46:01 +0800

    add zuul routes configuration

--- src/main/java/com/cloud/nsk/microservicegatewayzuul/MicroserviceGatewayZuulApplication.java
+++ src/main/java/com/cloud/nsk/microservicegatewayzuul/MicroserviceGatewayZuulApplication.java
@@ -1,14 +1,29 @@
 package com.cloud.nsk.microservicegatewayzuul;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroserviceGatewayZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceGatewayZuulApplication.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean getServlet(){
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
 	}
 }
