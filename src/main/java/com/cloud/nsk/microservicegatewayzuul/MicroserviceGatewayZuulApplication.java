555a78aed3d8e3d2a515209700547533269b7c68
nieshankun <38554448+nieshankun@users.noreply.github.com>
2018-11-11 22:27:40 +0800

    add zuul fallback provider

--- src/main/java/com/cloud/nsk/microservicegatewayzuul/MicroserviceGatewayZuulApplication.java
+++ src/main/java/com/cloud/nsk/microservicegatewayzuul/MicroserviceGatewayZuulApplication.java
@@ -5,12 +5,14 @@ import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServl
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceGatewayZuulApplication {
 
---
+++ src/main/java/com/cloud/nsk/microservicegatewayzuul/fallback/MovieFallbackProvider.java
@@ -0,0 +1,65 @@
+package com.cloud.nsk.microservicegatewayzuul.fallback;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+/**
+ * @author nsk
+ * 2018/11/11 21:54
+ */
+@Component
+public class MovieFallbackProvider implements FallbackProvider {
+    @Override
+    public String getRoute() {
+        return "microservice-consumer-movie";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return this.getStatusCode().value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return this.getStatusCode().getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream(("The microservice-consumer-movie is not available,please try it later!").
+                        getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                MediaType mt = new MediaType("application","json",
+                        Charset.forName("UTF-8"));
+                headers.setContentType(mt);
+                return headers;
+            }
+        };
+    }
+}
